035db2e36f6270cbcda2e265ad3e524608d5251f
salsapunk <bernardinhogamespray@gmail.com>
2025-04-11 14:45:42 -0300

    feito 7 add 8

--- 7substituir_letra.py
+++ 7substituir_letra.py
@@ -1,10 +1,19 @@
 def substituir_letra():
     texto = input('Digite a palavra: ')
-    letra_mudavel = input('Digite a letra que será substituida: ')
-    letra_substitui = input('Digite a letra que substituirá a letra escolhida: ')
-
-    palavras = [palavra for palavra in texto if palavra != "e"]
+        #pega o input da palavra
+    letra_m = input('Digite a letra que será substituida: ')
+        #pega o input da letra que será substituida
+    letra_s = input('Digite a letra que substituirá a letra escolhida: ')
+        #pega o input da letra que vai substituir
+    lista_letras = [caractere for caractere in texto]
+        #caractere para cada caractere em texto, isso separa os caracteres da string
+    palavras = [letra_s if caractere == letra_m else caractere for caractere in lista_letras]
+        #se o caractere for igual à letra_m, na lista palavras ele será letra_m
+        #se não, ele será igual à lista_letras, ou seja, não sofrem mudança
     resultado = ''.join(palavras)
+        #junta as letras separadas em lista numa string
     print(resultado)
+    #printa o resultado
 
 substituir_letra()
+    #chama a função
---
+++ 8primeira_ocorrencia_string.py
@@ -0,0 +1,10 @@
+texto = input('Digite o texto que deseja verificar: ')
+valor = input('Digite a letra que deseja saber: ')
+
+# lista = [letra for letra in texto]
+#     #transforma o texto numa lista de caracteres
+
+# if valor == letra:
+    
+
+# print(lista[letra])
